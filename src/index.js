602cb3f1ef2abe4a61ebb8c89d15066236b224e2
Jiachun Xiang <max997123@gmail.com>
2023-04-24 18:49:39 -0400

    create an array of tasks and write a function to iterate over the tasks array and populate an HTML list item element for each task

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const tasks = [
+    { description: 'Task 1', completed: false, index: 1 },
+    { description: 'Task 2', completed: true, index: 2 },
+    { description: 'Task 3', completed: false, index: 3 },
+  ];
+  
+  function renderTasks() {
+    const todoList = document.getElementById('todo-list');
+  
+    tasks.sort((a, b) => a.index - b.index);
+  
+    tasks.forEach((task) => {
+      const taskItem = document.createElement('li');
+      taskItem.textContent = task.description;
+  
+      if (task.completed) {
+        taskItem.classList.add('completed');
+      }
+  
+      todoList.appendChild(taskItem);
+    });
+  }
+  
+  renderTasks();
+  
