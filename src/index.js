9f1a503e78c3ba1778dcd2c00e78d6953365e416
Jiachun Xiang <max997123@gmail.com>
2023-05-01 10:33:35 -0400

    make css work and move index.html location

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "style.css";
+
 const tasks = [
     { description: 'Task 1', completed: false, index: 1 },
     { description: 'Task 2', completed: true, index: 2 },
--- webpack.config.js
+++ webpack.config.js
@@ -19,4 +19,12 @@ module.exports = {
       template: './index.html',
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
