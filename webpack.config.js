a63b82fbd72cbb1923e36b0b640121ffe7d08f25
Jiachun Xiang <max997123@gmail.com>
2023-05-03 18:47:23 -0400

    add output clean for console error

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    assetModuleFilename: '[name][ext]',
   },
   devServer: {
     static: path.join(__dirname, 'dist'),
