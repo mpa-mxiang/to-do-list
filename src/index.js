584eb291dec897b85f1cd3e62bcef34803af0633
Jiachun Xiang <max997123@gmail.com>
2023-05-03 11:55:51 -0400

    fix eslint errors with console and arrow function

--- src/index.js
+++ src/index.js
@@ -1,41 +1,20 @@
 import './style.css';
 import './storage.js';
+
 let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-function renderTasks() {
-  const todoList = document.getElementById('todo-list');
-  todoList.innerHTML = ''; // clear previous items
-
-  tasks.sort((a, b) => a.index - b.index);
-
-  tasks.forEach((task, index) => {
-    const taskItem = document.createElement('li');
-    taskItem.innerHTML += `<input class='checkbox' type="checkbox" ${task.completed ? 'checked' : ''}>${task.description}</input> <i class="fa">&#xf142;</i>`;
-    taskItem.querySelector('input').addEventListener('click', () => toggleTaskCompletion(index));
-    taskItem.querySelector('p').addEventListener('click', () => editTask(index));
-
-    if (task.completed) {
-      taskItem.classList.add('completed');
-    }
-
-    todoList.appendChild(taskItem);
-  });
+function saveTasks() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-function addTask(description) {
-  const task = {
-    description: description,
-    completed: false,
-    index: tasks.length + 1
-  };
-
-  tasks.push(task);
+function toggleTaskCompletion(index) {
+  tasks[index].completed = !tasks[index].completed;
   saveTasks();
   renderTasks();
 }
 
 function editTask(index) {
-  const taskItem = document.querySelector(`#todo-list li:nth-child(${index+1})`);
+  const taskItem = document.querySelector(`#todo-list li:nth-child(${index + 1})`);
   const descriptionElement = taskItem.querySelector('p');
   const editInput = document.createElement('input');
   editInput.type = 'text';
@@ -49,14 +28,36 @@ function editTask(index) {
   editInput.focus();
 }
 
-function toggleTaskCompletion(index) {
-  tasks[index].completed = !tasks[index].completed;
-  saveTasks();
-  renderTasks();
+function renderTasks() {
+  const todoList = document.getElementById('todo-list');
+  todoList.innerHTML = ''; // clear previous items
+
+  tasks.sort((a, b) => a.index - b.index);
+
+  tasks.forEach((task, index) => {
+    const taskItem = document.createElement('li');
+    taskItem.innerHTML += `<input class='checkbox' type="checkbox" ${task.completed ? 'checked' : ''}/><p>${task.description}</p> <i class="fa">&#xf142;</i>`;
+    taskItem.querySelector('input').addEventListener('click', () => toggleTaskCompletion(index));
+    taskItem.querySelector('p').addEventListener('click', () => editTask(index));
+
+    if (task.completed) {
+      taskItem.classList.add('completed');
+    }
+
+    todoList.appendChild(taskItem);
+  });
 }
 
-function saveTasks() {
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+function addTask(description) {
+  const task = {
+    description,
+    completed: false,
+    index: tasks.length + 1,
+  };
+
+  tasks.push(task);
+  saveTasks();
+  renderTasks();
 }
 
 const newTaskForm = document.querySelector('form');
@@ -70,7 +71,9 @@ newTaskForm.addEventListener('submit', (event) => {
 const clearCompletedBtn = document.querySelector('.clear-completed-btn');
 clearCompletedBtn.addEventListener('click', () => {
   tasks = tasks.filter((task) => !task.completed);
-  tasks.forEach((task, i) => task.index = i + 1);
+  tasks.forEach((task, i) => {
+    task.index = i + 1;
+  });
   saveTasks();
   renderTasks();
 });
