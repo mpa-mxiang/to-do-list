d5fd2211220184c4f3dcd84aa83dd1f0a84a24f8
Jiachun Xiang <max997123@gmail.com>
2023-05-05 12:27:42 -0400

    localstorage to the right place to make refresh work

--- src/update.js
+++ src/update.js
@@ -1,6 +1,6 @@
 export function updateStatus(tastList, index, completed) {
   tastList[index].completed = completed;
-  localStorage.setItem('tastList', JSON.stringify(tastList));
+  localStorage.setItem('tasks', JSON.stringify(tastList));
 }
 
 export function clearCompleted(tastList) {
@@ -10,6 +10,6 @@ export function clearCompleted(tastList) {
     task.index = i + 1;
   });
 
-  localStorage.setItem('tastList', JSON.stringify(tastList));
+  localStorage.setItem('tasks', JSON.stringify(tastList));
   return tastList;
 }
