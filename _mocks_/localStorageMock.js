6e2c86cdb01b043fa76385cb93ae36f193fdf5b9
Jiachun Xiang <max997123@gmail.com>
2023-05-09 11:42:59 -0400

    change mock localstorage

--- _mocks_/localStorageMock.js
+++ _mocks_/localStorageMock.js
@@ -1,16 +1,22 @@
 const localStorageMock = (() => {
-    let store = {};
-  
-    return {
-      getItem: (key) => store[key] || {},
-      setItem: (key, value) => (store[key] = value.toString()),
-      clear: () => (store = {}),
-      removeItem: (key) => delete store[key],
-    };
-  })();
-  
-  // In your test, you can replace the `localStorage` object with the mock
-  Object.defineProperty(window, 'localStorage', {
-    value: localStorageMock,
-  });
-  
+  let store = {};
+
+  return {
+    getItem: (key) => {
+      console.log(`Getting item ${key} from mock storage`);
+      return store[key] || {};
+    },
+    setItem: (key, value) => {
+      console.log(`Setting item ${key} to ${value} in mock storage`);
+      store[key] = value.toString();
+    },
+    clear: () => {
+      console.log(`Clearing mock storage`);
+      store = {};
+    },
+    removeItem: (key) => {
+      console.log(`Removing item ${key} from mock storage`);
+      delete store[key];
+    },
+  };
+})();
