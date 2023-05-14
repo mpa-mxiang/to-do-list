a552a0d829e3e385c48ddf591a1d8718e828dfc1
Jiachun Xiang <max997123@gmail.com>
2023-05-14 14:05:09 -0400

    add some console try to find the issue

--- _tests_/app.test.js
+++ _tests_/app.test.js
@@ -5,11 +5,10 @@ import 'jest-localstorage-mock';
 
 const tastList = tasks;
 let todoList;
-
+let taskItem;
 const renderTasks = jest.fn(() => {
   tastList.sort((a, b) => a.index - b.index);
   tastList.forEach((task) => {
-    const taskItem = document.createElement('li');
     taskItem.innerHTML += `
       <input class='checkbox' type="checkbox" ${task.completed ? 'checked' : ''}/>
         <p>${task.description}</p>
@@ -17,7 +16,10 @@ const renderTasks = jest.fn(() => {
           <i class="fa tri-dots">&#xf142;</i>
           <i class="fa fa-trash-o trash"></i>
         </div>`;
+    console.log("render:" + taskItem.textContent)
     todoList.appendChild(taskItem);
+    console.log("render:" + todoList)
+
   });
 });
 
@@ -35,6 +37,9 @@ describe('addTask function', () => {
     global.window = dom.window;
     global.document = dom.window.document;
     todoList = document.getElementById('todo-list');
+    taskItem = document.createElement('li');
+    console.log("before:" + taskItem.textContent)
+    console.log("before:" + todoList)
   });
 
   afterEach(() => {
@@ -42,6 +47,9 @@ describe('addTask function', () => {
     delete global.document;
     localStorage.clear();
     jest.clearAllMocks();
+
+    console.log("after:" + taskItem.textContent)
+    console.log("after:" + todoList)
   });
 
   test('adds a new task to the list and localStorage', () => {
@@ -54,5 +62,18 @@ describe('addTask function', () => {
 
     expect(todoList.children.length).toBe(1);
     expect(todoList.children[0].querySelector('p').textContent).toBe('Task 1');
+  });
+
+  test('deletes a task from the list and localStorage', () => {
+    addTask('Task 1');
+    addTask('Task 2');
+    renderTasks();
+    const updatedTastList = JSON.parse(localStorage.getItem('tasks'));
+    console.log(updatedTastList)
+    // check that the new task was added to the tastList and localStorage
+    expect(updatedTastList).toEqual([{ description: 'Task 1', completed: false, index: 1 },{ description: 'Task 2', completed: false, index: 2 }]);
+
+    expect(todoList.children.length).toBe(2);
+    expect(todoList.children[1].querySelector('p').textContent).toBe('Task 2');
   });
 });
