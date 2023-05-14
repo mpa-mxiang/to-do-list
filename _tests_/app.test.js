f413ae12a9334417ff4d2ab884d1384724b564ab
Jiachun Xiang <max997123@gmail.com>
2023-05-14 14:31:59 -0400

    restore changes

--- _tests_/app.test.js
+++ _tests_/app.test.js
@@ -3,43 +3,39 @@ import { addTask } from '../src/app.js';
 import tasks from '../src/tasks.js';
 import 'jest-localstorage-mock';
 
-const tastList = tasks;
+const taskList = tasks;
 let todoList;
 let taskItem;
 const renderTasks = jest.fn(() => {
-  tastList.sort((a, b) => a.index - b.index);
-  tastList.forEach((task) => {
+  taskList.sort((a, b) => a.index - b.index);
+  taskList.forEach((task) => {
     taskItem.innerHTML += `
       <input class='checkbox' type="checkbox" ${task.completed ? 'checked' : ''}/>
-        <p>${task.description}</p>
-        <div class="icons">
-          <i class="fa tri-dots">&#xf142;</i>
-          <i class="fa fa-trash-o trash"></i>
-        </div>`;
-    console.log("render:" + taskItem.textContent)
+      <p>${task.description}</p>
+      <div class="icons">
+        <i class="fa tri-dots">&#xf142;</i>
+        <i class="fa fa-trash-o trash"></i>
+      </div>`;
     todoList.appendChild(taskItem);
-    console.log("render:" + todoList)
-
   });
 });
 
 describe('addTask function', () => {
   beforeEach(() => {
     const dom = new JSDOM(`
-    <!DOCTYPE html>
-    <html>
-      <body>
-        <ul id="todo-list"></ul>
-      </body>
-    </html>
+      <!DOCTYPE html>
+      <html>
+        <body>
+          <ul id="todo-list"></ul>
+        </body>
+      </html>
     `);
 
     global.window = dom.window;
     global.document = dom.window.document;
     todoList = document.getElementById('todo-list');
     taskItem = document.createElement('li');
-    console.log("before:" + taskItem.textContent)
-    console.log("before:" + todoList)
+    tasks.length = 0; // Clear the tasks array before each test
   });
 
   afterEach(() => {
@@ -47,18 +43,14 @@ describe('addTask function', () => {
     delete global.document;
     localStorage.clear();
     jest.clearAllMocks();
-
-    console.log("after:" + taskItem.textContent)
-    console.log("after:" + todoList)
   });
 
   test('adds a new task to the list and localStorage', () => {
     addTask('Task 1');
     renderTasks();
-    const updatedTastList = JSON.parse(localStorage.getItem('tasks'));
+    const updatedTaskList = JSON.parse(localStorage.getItem('tasks'));
 
-    // check that the new task was added to the tastList and localStorage
-    expect(updatedTastList).toEqual([{ description: 'Task 1', completed: false, index: 1 }]);
+    expect(updatedTaskList).toEqual([{ description: 'Task 1', completed: false, index: 1 }]);
 
     expect(todoList.children.length).toBe(1);
     expect(todoList.children[0].querySelector('p').textContent).toBe('Task 1');
@@ -68,12 +60,14 @@ describe('addTask function', () => {
     addTask('Task 1');
     addTask('Task 2');
     renderTasks();
-    const updatedTastList = JSON.parse(localStorage.getItem('tasks'));
-    console.log(updatedTastList)
-    // check that the new task was added to the tastList and localStorage
-    expect(updatedTastList).toEqual([{ description: 'Task 1', completed: false, index: 1 },{ description: 'Task 2', completed: false, index: 2 }]);
 
-    expect(todoList.children.length).toBe(2);
-    expect(todoList.children[1].querySelector('p').textContent).toBe('Task 2');
+    deleteTask(0);
+    renderTasks();
+    const updatedTaskList = JSON.parse(localStorage.getItem('tasks'));
+
+    expect(updatedTaskList).toEqual([{ description: 'Task 2', completed: false, index: 1 }]);
+
+    expect(todoList.children.length).toBe(1);
+    expect(todoList.children[0].querySelector('p').textContent).toBe('Task 2');
   });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'open',
   entry: './src/index.js',
   output: {
     filename: './dist/main.js',
